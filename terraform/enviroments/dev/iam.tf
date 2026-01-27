8ee36a8b5f6447c0bee188fdff3ac9430b3416d4
barispolatt <barispolat1999@hotmail.com>
2026-01-27 23:43:51 +0300

    iam.tf added

--- terraform/enviroments/dev/iam.tf
+++ terraform/enviroments/dev/iam.tf
@@ -14,20 +14,20 @@ resource "aws_iam_role_policy_attachment" "codedeploy_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforAWSCodeDeploy"
 }
 
-# 3. S3 access policy (CodeDeploy Artifactleri için)
+# S3 access policy (CodeDeploy Artifactleri için)
 resource "aws_iam_role_policy_attachment" "s3_read_policy" {
   role       = aws_iam_role.ec2_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
 }
 
-# 4. SSM access policy (to read api keys) 
+# SSM access policy (to read api keys) 
 # The Spring Boot application will read the Spotify API Key from here when it starts.
 resource "aws_iam_role_policy_attachment" "ssm_read_policy" {
   role       = aws_iam_role.ec2_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
-# 5. Instance Profile (Bridge between role and EC2)
+# Instance Profile (Bridge between role and EC2)
 resource "aws_iam_instance_profile" "ec2_profile" {
   name = "${var.environment}-ec2-profile"
   role = aws_iam_role.ec2_role.name
