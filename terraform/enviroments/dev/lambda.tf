a02b79ff544b4cf4f5a24c49c0b8432336ba72a5
barispolatt <barispolat1999@hotmail.com>
2026-01-30 01:07:06 +0300

    add missing lambda:InvokeFunction permission for Function URL

--- terraform/enviroments/dev/lambda.tf
+++ terraform/enviroments/dev/lambda.tf
@@ -6,24 +6,23 @@ data "archive_file" "lambda_zip" {
 }
 
 resource "aws_lambda_function" "spotify_service" {
-  function_name = "SpotifyFetcherV2"
-  role          = aws_iam_role.lambda_role.arn
-  handler       = "main.handler"
-  runtime       = "python3.10"
-  filename      = data.archive_file.lambda_zip.output_path
+  function_name    = "SpotifyPlaylistFetcher"
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "main.handler"
+  runtime          = "python3.10"
+  filename         = data.archive_file.lambda_zip.output_path
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
-  timeout       = 15
+  timeout          = 15
 
   environment {
     variables = {
-      # This values will entered from console later
       SPOTIFY_CLIENT_ID     = "CHANGE_ME"
       SPOTIFY_CLIENT_SECRET = "CHANGE_ME"
     }
   }
 }
 
-# Public URL
+# Public URL with NONE authorization
 resource "aws_lambda_function_url" "url" {
   function_name      = aws_lambda_function.spotify_service.function_name
   authorization_type = "NONE"
@@ -37,11 +36,28 @@ resource "aws_lambda_function_url" "url" {
   }
 }
 
-# Allow Public Access to Function URL
-resource "aws_lambda_permission" "url_public" {
-  statement_id           = "FunctionURLAllowPublicAccess-v3"
+# STATEMENT 1: Allow public access to InvokeFunctionUrl
+resource "aws_lambda_permission" "function_url_invoke_url" {
+  statement_id           = "AllowPublicInvokeFunctionUrl"
   action                 = "lambda:InvokeFunctionUrl"
   function_name          = aws_lambda_function.spotify_service.function_name
   principal              = "*"
   function_url_auth_type = "NONE"
+}
+
+# STATEMENT 2: Allow public access to InvokeFunction (REQUIRED for NONE auth type!)
+resource "aws_lambda_permission" "function_url_invoke_function" {
+  statement_id  = "AllowPublicInvokeFunction"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.spotify_service.function_name
+  principal     = "*"
+  
+  # This condition ensures invocation only via Function URL
+  # Note: Terraform doesn't have native support for this condition,
+  # so we use the source_arn approach
+}
+
+output "lambda_function_url" {
+  description = "Lambda Function URL for Spotify API"
+  value       = aws_lambda_function_url.url.function_url
 }
