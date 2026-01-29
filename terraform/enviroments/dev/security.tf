304ed36999a241a408651edf1ea209a372652f8c
barispolatt <barispolat1999@hotmail.com>
2026-01-29 04:25:14 +0300

    security update for development

--- terraform/enviroments/dev/security.tf
+++ terraform/enviroments/dev/security.tf
@@ -9,7 +9,7 @@ resource "aws_security_group" "k3s_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.my_ip] 
+    cidr_blocks = ["0.0.0.0/0"] # Allowed for GitHub Actions (Temporary Dev Fix) 
   }
 
   # HTTP Access
